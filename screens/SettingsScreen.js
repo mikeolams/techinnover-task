b9ae5597634f486b6d68f7ad53ae787b63813575
GMike the GD <mikeolams@yahoo.com>
2020-07-08 09:49:09 +0100

    adjust setting

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -3,312 +3,343 @@ import {
     Image,
     Platform,
     StyleSheet,
-    Text,
-    TouchableOpacity,
     View,
     AsyncStorage
 } from 'react-native';
-import Header from '../components/Header';
+import { WebView } from 'react-native-webview';
 
-export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  // return <ExpoConfigView />;
-  const [settingProfile, setSettingProfile] = useState('');
-  const [dataLoaded, setDataLoaded] = useState(false);
+export default function SettingsScreen(props) {
 
-  const getProfile = (token)=> {
+    const [userInfo, setUserInfo] = useState(''),
+     [token, setToken] = useState('');
+    let sum=0;
+  
+    const loadSetting = ()=>{
+        return <WebView originWhitelist={['*']} 
+        style={styles.webPostion}
+        onNavigationStateChange={handleWebView}
+        source={{uri:'https://farmcenta.com/api/v1/profile?token='+token}}/>
+    }
+    
+    const retrieveUserInfo = async () => {
+      try {
+        const name = await AsyncStorage.getItem("user"),
+    tokenKey= await AsyncStorage.getItem("token"),
+    avatar= await AsyncStorage.getItem("avatar");
+    setToken(tokenKey);
+    if (avatar !== null) {
+      // You can access your data
+      setUserInfo([name, avatar])
+      console.log(userInfo)
+  
+    }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-        fetch('https://farmcenta.com/api/v1/profile?token='+token,{
-          method: 'POST',
-          header: {
-            'Content-Type': 'application/json'
-          }
-        })
-        .then(res => res.json())
-        .then(profile => {
-          
-        // console.log(profile);
-        console.log(token);
-        setSettingProfile(profile);
-        setDataLoaded(true)
-        console.log(settingProfile);
-      })
-      .catch(err=> {
-        console.warn('issues fetching profile parameters '+err )
-      });
-};
+     useEffect(() => {
+           retrieveUserInfo();
+        }, []);
+        
 
-const retrieveUserToken = async () => {
-  try {
-    const token= await AsyncStorage.getItem("token");
-getProfile(token);
-// if (avatar !== null) {
-//   // You can access your data
-//   setUserInfo([name, avatar])
-//   console.log(userInfo)
+         const  handleWebView = (newNavState) => {    
+        // console.log(newNavState);
+            const { canGoBack } = newNavState;
+            if (!canGoBack) return;
+            console.log(canGoBack)
+            }
 
-// }
-  } catch (error) {
-    console.log(error);
-  }
-};
+    return (
+        <View style={styles.container}>
 
-useEffect(() => {
-  retrieveUserToken();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-        <View style={styles.innerContainer}>
-            <View style={styles.topContainer} >
-              <TouchableOpacity style={styles.avatarImage}>
-              <Image 
-                    source={
-                      require('../assets/images/avatar-logo.png')
-                  }
-                //   source={
-                //     require('../assets/images/logo.png')
-                // }
-                    // style={styles.logoImage}
-                    style={styles.image}
-                />
-              </TouchableOpacity>
-            </View>
-            {dataLoaded?<View style={styles.bottomContainer}>
-              <TouchableOpacity style={styles.itemContainer}>
-                <Text style={styles.text}>Name</Text><Text style={styles.text}>></Text>
-              </TouchableOpacity>
-              <TouchableOpacity  style={styles.itemContainer}>
-                <Text style={styles.text}>Mobile:</Text><Text style={styles.text}>{settingProfile.profile[0].mobile}</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.itemContainer}><Text style={styles.text}>Date of Birth:</Text><Text style={{...styles.text1,...styles.sign}}>{settingProfile.profile[0].dob}</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.itemContainer}><Text style={styles.text}>Gender:</Text><Text style={{...styles.text1,...styles.sign}}>{settingProfile.profile[0].gender}</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.itemContainer}><Text style={styles.text}>State of Origin:</Text><Text style={{...styles.text1,...styles.sign}}>{settingProfile.profile[0].state}</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.itemContainer}><Text style={styles.text}>Bank</Text><Text style={{...styles.text1,...styles.sign}}>{settingProfile.profile[0].bank}</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.itemContainer}><Text style={styles.text}>Account Number:</Text><Text style={styles.text1}>{settingProfile.profile[0].bank_number}</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.itemContainer}><Text style={styles.text}>Other Bank Details:</Text><Text style={{...styles.text1,...styles.sign}}>{settingProfile.profile[0].other_details}></Text></TouchableOpacity>
-              <TouchableOpacity style={styles.itemContainer}><Text style={styles.text}>Next of Kin Name:</Text><Text style={{...styles.text1,...styles.sign}}>{settingProfile.profile[0].next_kin}</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.itemContainer}><Text style={styles.text}>Next of Kin's Number:</Text><Text style={{...styles.text1,...styles.sign}}>0{settingProfile.profile[0].next_kin_number}</Text></TouchableOpacity>
-            </View>:null}
+            <View style={styles.innerContainer}>
+            
+            <View>
+                <View style={styles.imageContainer} >
+                    <Image
+                        source={
+                            require('../assets/images/logo.png')
+                        }
+                        style={styles.logoImage}
+                    />
+                </View>
+                <View style={styles.id}>
+           </View>
+           </View>
+           {loadSetting()}
+            </View> 
         </View>
-
-    </View>
-);
+    );
 }
 
 SettingsScreen.navigationOptions = {
-  // title: 'app.json',
-  // header: null,
-  header: Header
+    header: null,
 };
 
 const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: '#fff',
-  },
-  innerContainer: {
-      flex: 1,
-      // backgroundColor: '#f0f',
-      width:'95%',
-      alignSelf:"center"
-  },
-  topContainer: {
-    flex:1,
-    // paddingTop: 1,
-    justifyContent:"center",
-    alignItems: 'center',
-    // backgroundColor: '#2f2',#0E861C33
-    // backgroundColor: '#0E861C33',
-},
-avatarImage:{
-  width: 90,
-      height: 90,
-      resizeMode: 'contain',
-      // marginTop:40
-},
-image:{
-  flex:1
-},
-bottomContainer: {
-  flex:3,
-  // paddingTop: 1,
-  alignItems: 'center',
-  // backgroundColor: '#df2',
-},
-itemContainer:{
-  flex:1,
-  flexDirection: "row",
-  // backgroundColor: '#03e',
-  width:"90%",
-  justifyContent:"space-between",
-  alignItems:"center",
-  borderBottomColor:"#0E861C33",
-  borderBottomWidth:1,
-  paddingHorizontal:20
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        // paddingTop:80
+    },
+    innerContainer: {
+        flex: 1,
+    },
+    contentContainer: {
+        alignItems: 'center',
+    },
+    productsContainer:{
+        flex: 1,
+        flexWrap:"wrap",
+        flexDirection: 'row',
+        justifyContent:'center'
+    },
+    itemContainer:{
+        alignItems: 'center',
+        width:'30%',
+        margin:4,
+        borderRadius:20
+    },
+    buttonContainer:{
+        justifyContent:'center',
+        paddingVertical: 20,
+        flexDirection: "row",
+      },
+      helpLinkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+        textAlign: 'center',
+      },
+    itemTitle:{
+        textAlign:'center',
+        fontWeight:'bold'
+    },
+    itemTextBelow:{
+        marginTop:15,
+    },
+    imageContainer: {
+        paddingTop: 1,
+        alignItems: 'center',
+    },
+    
+    rowContainer: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        // paddingHorizontal: 15,
+    },
+      leftButton:{
+        borderBottomLeftRadius:10,
+        borderTopLeftRadius:10,
+      },
+      rightButton:{
+        borderBottomRightRadius:10,
+        borderTopRightRadius:10,
+      },
+      buttonSet: {
+        justifyContent:'center',
+        // backgroundColor: '#0C9121',
+        width:"40%",
+        height:20,
+        borderColor:'#2e78b7',
+        borderWidth:1,
+      },
+      buttonText: {
+        fontSize: 14,
+        lineHeight: 24,
+        textAlign: 'center',
+      },
+    text: {
+        color: 'white',
+    },
+    boldText: {
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    mouText: {
+        color: 'red',
+        fontSize:10
+    },
+    mouTick:{
+        backgroundColor:'#0E861C',
+        width:30,
+        height:30,
+        justifyContent:"center",
+        alignItems:"center",
+        borderRadius:4
+    },
+    content2Container: {
+        flex: 1,
+        paddingTop:10,
+        marginVertical: 15,
+        justifyContent: 'space-between',
+        width: '100%',
+    },
 
-},
-text:{
-  // textAlign:'center',
-  fontWeight:'bold',
-  color:"#0C9121"
-},
-// sign:{
-//   fontSize:10
-// },
-
-
-
-  contentContainer: {
-      alignItems: 'center',
-  },
-  productsContainer:{
-      flex: 1,
-      flexWrap:"wrap",
-      flexDirection: 'row',
-      // backgroundColor:'yellow',
-      justifyContent:'center'
-  },
-  // itemContainer:{
-  //     alignItems: 'center',
-  //     width:'30%',
-  //     margin:4,
-  //     borderRadius:20
-  // },
-  buttonContainer:{
-      // flex:1,
-      justifyContent:'center',
-      // alignItems: 'center',
-      paddingVertical: 20,
-      flexDirection: "row",
+    text2: {
+        color: '#0E861C',
+        fontWeight:'bold'
     },
-    helpLinkText: {
-      fontSize: 14,
-      color: '#2e78b7',
-      textAlign: 'center',
+    logoImage: {
+        width: 180,
+        height: 50,
+        resizeMode: 'contain',
+        marginTop:40
     },
-  itemTitle:{
-      textAlign:'center',
-      fontWeight:'bold'
-  },
-  itemTextBelow:{
-      marginTop:15,
-  },  
-  rowContainer: {
-      flexDirection: "row",
-      justifyContent: 'space-between',
-      // paddingHorizontal: 15,
-  },
-  closeButton: {
-      // paddingVertical: 15,
-      justifyContent:'center',
-      backgroundColor: '#0C9121',
-      width:"40%",
-      height:20,
-      borderBottomRightRadius:10,
-      borderTopRightRadius:10,
+    itemImage: {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain',
+        margin:10,
+        borderRadius:50
     },
-    sellingButton: {
-      // paddingVertical: 15,
-      borderBottomLeftRadius:10,
-      borderTopLeftRadius:10,
-      justifyContent:'center',
-      backgroundColor: '#fff',
-      width:'40%',
-      height:20,
-      borderColor:'#2e78b7',
-      borderWidth:1,
-      // borderRadius:10
+    picImage:{
+        width: 30,
+        height: 30,
+        resizeMode: 'contain',
+        marginLeft:10,
+        borderRadius:20
+      },
+      id:{
+        paddingHorizontal:10,
+        justifyContent:"center",
+        marginLeft:18,
+        width:'30%',
+        zIndex:2,
+        top:-3
+      },
+      backButton:{
+          alignItems:"flex-end",
+          marginLeft:'80%',
+          marginRight:10
+      },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: { width: 0, height: -3 },
+                shadowOpacity: 0.1,
+                shadowRadius: 3,
+            },
+            android: {
+                elevation: 20,
+            },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
     },
-    closeButtonText: {
-      fontSize: 14,
-      color: '#fff',
-      lineHeight: 24,
-      textAlign: 'center',
+    tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center',
     },
-  // text: {
-  //     color: 'white',
-  // },
-  boldText: {
-      color: 'white',
-      fontWeight: 'bold'
-  },
-  content2Container: {
-      flex: 1,
-      paddingTop:10,
-      marginVertical: 15,
-      justifyContent: 'space-between',
-      width: '100%',
-  },
-
-  text2: {
-      color: '#0E861C',
-      fontWeight:'bold'
-  },
-  itemImage: {
-      width: 100,
-      height: 100,
-      resizeMode: 'contain',
-      margin:10,
-      borderRadius:50
-  },
-  picImage:{
-      width: 30,
-      height: 30,
-      resizeMode: 'contain',
-      marginLeft:10,
-      borderRadius:20
+    navigationFilename: {
+        marginTop: 5,
     },
-    id:{
-      paddingHorizontal:10,
-      justifyContent:"center",
-      marginLeft:18,
-      width:'30%',
-      zIndex:2,
-      top:-3
+    helpContainer: {
+        marginTop: 15,
+        alignItems: 'center',
     },
-  tabBarInfoContainer: {
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      ...Platform.select({
-          ios: {
-              shadowColor: 'black',
-              shadowOffset: { width: 0, height: -3 },
-              shadowOpacity: 0.1,
-              shadowRadius: 3,
-          },
-          android: {
-              elevation: 20,
-          },
-      }),
-      alignItems: 'center',
-      backgroundColor: '#fbfbfb',
-      paddingVertical: 20,
-  },
-  tabBarInfoText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      textAlign: 'center',
-  },
-  navigationFilename: {
-      marginTop: 5,
-  },
-  helpContainer: {
-      marginTop: 15,
-      alignItems: 'center',
-  },
-  helpLink: {
-      paddingVertical: 15,
-  },
-  sellingButtonText: {
-      fontSize: 14,
-      color: '#2e78b7',
-      lineHeight: 24,
-      textAlign: 'center',
-  },
+    helpLink: {
+        paddingVertical: 15,
+    },
+    productTop:{
+        flexDirection:"row",
+        // flex:1,
+        justifyContent:"center",
+        borderTopColor:"#0E861C33",
+        borderTopWidth:2,
+        height:180,
+        paddingTop:20,
+        paddingHorizontal:20,
+    },
+    productHead:{
+        flex:1,
+        justifyContent:"center",
+    },
+    productDetails:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:'center',
+        paddingVertical:30
+    },
+    productBelow:{
+        paddingTop:20,
+        justifyContent:"center"
+    },
+    productTitle:{
+        paddingHorizontal:30,
+        borderBottomColor:"#0E861C33",
+        borderBottomWidth:1,
+    },
+    productData:{
+        justifyContent:'space-between',
+        flexDirection:"row",
+        paddingHorizontal:30,
+        borderBottomColor:"#0E861C33",
+        borderBottomWidth:1,
+        alignItems:"center",
+        minHeight:45
+    },
+    select:{
+        justifyContent:'space-between',
+        flexDirection:"row",
+        paddingHorizontal:5,
+        borderWidth:1,
+        borderRadius:5,
+        alignItems:"center",
+        minHeight:30,
+        width:70
+    },
+    productButton:{
+        justifyContent:"space-evenly",
+        alignItems:"center",
+        backgroundColor:'#0E861C',
+        minHeight:35,
+        margin:30,
+        flexDirection:"row",
+    },
+    payButtons:{
+        flexDirection:"row",
+        justifyContent:"center",
+    },
+    productClear:{
+        backgroundColor:'red',
+        alignSelf:"flex-end",
+        marginRight:30,
+        marginVertical:10,
+        minHeight:25,
+        width:80,
+        justifyContent:"center"
+    },
+    list:{
+        flexDirection:"row",
+        flex:1
+    },
+    listImage:{
+        width: 150,
+        height: 150,
+        resizeMode: 'contain',
+        marginVertical:10
+    },
+    listText: {
+        color: '#0E861C',
+    },
+    listAmount:{
+        fontSize:38,
+        fontWeight:"bold"
+    },
+    listName:{
+        fontSize:25,
+        color: '#0E861C',
+    }
 });
+
+
+
+
